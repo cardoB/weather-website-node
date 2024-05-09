b2525009e5993d0d8afa0974b5d8c8a477e00b0c
Riccardo Belletta <riccardo.belletta@gmail.com>
2024-05-09 14:46:58 -0400

    Removed console.log

--- public/js/client-app.js
+++ public/js/client-app.js
@@ -1,5 +1,3 @@
-console.log('hi');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('.location-input');
 const messageOne = document.querySelector('#message-1');
